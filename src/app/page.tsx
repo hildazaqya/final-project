a1b87f9b82847698bda96b22e6c6eb02cfa3a453
hildazaqyaelnaz <hildazaqyaelnaz@gmail.com>
2024-06-25 13:00:58 +0000

    add popup youtube

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Hero from "@/components/ui/hero";
 import TrendingNow from "@/components/ui/trending";
 import LastTrailer from "@/components/ui/lasttrailer";
 import NowPlaying from "@/components/ui/nowplaying";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default async function MoviePage() {
   return (
--- src/components/ui/lasttrailer.tsx
+++ src/components/ui/lasttrailer.tsx
@@ -5,6 +5,9 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import Modal from 'react-modal';
 
 const API_KEY = '4f23342c64119b888d4db574dbbab573';
 
@@ -23,8 +26,9 @@ async function getMovieDetails(movieId: number) {
 export default function LastTrailer() {
   const linkImages = "https://image.tmdb.org/t/p/w500";
   const [trailers, setTrailers] = useState<any[]>([]);
-  const [hoveredTrailerId, setHoveredTrailerId] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentVideoKey, setCurrentVideoKey] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -49,29 +53,36 @@ export default function LastTrailer() {
     fetchData();
   }, []);
 
+  const openModal = (videoKey: string) => {
+    setCurrentVideoKey(videoKey);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setCurrentVideoKey(null);
+  };
+
   if (error) {
     return <div className="text-white">Error: {error}</div>;
   }
 
-  // if (trailers.length === 0) {
-  //   return <div className="text-white">Loading...</div>;
-  // }
-
   return (
     <div className="flex flex-col justify-center z-10 mt-5 ps-10 py-5 bg-marimo">
       <div className="mx-[50px]">
-      <div className="title flex justify-start items-center w-full">
-        <h3 className="text-2xl font-bold text-white text-left">
-          Latest Trailer
-        </h3>
-      </div>
-      <div className="relative flex flex-row gap-5 z-10 items-center justify-center !w-[1000px] mt-4">
-        <Swiper spaceBetween={50} slidesPerView={4}>
-          {trailers.map((item: any) => (
-            <SwiperSlide key={item.id}>
-              <div className="relative flex flex-col items-center justify-center p-2 rounded-md hover:scale-105 transition-transform duration-300"
-              >
-                <Link href={`https://www.youtube.com/watch?v=${item.key}`}>
+        <div className="title flex justify-start items-center w-full">
+          <h3 className="text-2xl font-bold text-white text-left">
+            Latest Trailer
+          </h3>
+        </div>
+        <div className="relative flex flex-row gap-5 z-10 items-center justify-center !w-[1000px] mt-4">
+          <Swiper spaceBetween={50} slidesPerView={4}>
+            {trailers.map((item: any) => (
+              <SwiperSlide key={item.id}>
+                <div
+                  className="relative flex flex-col items-center justify-center p-2 rounded-md hover:scale-105 transition-transform duration-300"
+                  onClick={() => openModal(item.key)}
+                >
                   <div className="block overflow-hidden w-[220px] !h-[124px]">
                     <Image
                       src={`${linkImages}${item.poster_path}`}
@@ -81,20 +92,40 @@ export default function LastTrailer() {
                       className="object-cover rounded-md !h-[124px]"
                     />
                     <div className="play absolute top-0 left-0 flex items-center justify-center w-[220px] h-buttonplay">
-                    <span>
-                    <Image width={48} height={48} src={`https://img.icons8.com/ios/96/circled-play--v1.png`} alt="circled-play--v1"/>
-                    </span>
+                      <span>
+                        <FontAwesomeIcon icon={faPlay} size="2xl" style={{ color: "#ffffff" }} />
+                      </span>
+                    </div>
                   </div>
-                  </div>
-                </Link>
-                <h2 className="text-white text-sm mt-2 text-left">{item.name}</h2>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+                  <h2 className="text-white text-sm mt-2 text-left">{item.name}</h2>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
-      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Trailer Modal"
+        className="flex items-center justify-center"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      >
+        <div className="bg-white rounded-lg w-[80vw] h-[80vh] flex items-center justify-center border-none">
+          <button onClick={closeModal} className="absolute top-2 right-2 text-black text-2xl">×</button>
+          {currentVideoKey && (
+            <iframe
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${currentVideoKey}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          )}
+        </div>
+      </Modal>
     </div>
   );
 }
-
