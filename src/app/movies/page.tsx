0d546cf4f25d0fab275134c18b6406ab4788c35b
hildazaqyaelnaz <hildazaqyaelnaz@gmail.com>
2024-07-13 12:04:37 +0000

    change ui design

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -21,7 +21,7 @@ export default async function Movies() {
   return (
     <section className="flex flex-col flex-wrap gap-4 bg-blacky items-center justify-center mx-[50px]">
       <div className="container h-80 flex items-center">
-        <div className="flex flex-col items-center justify-start text-center md:text-left space-y-4 text-white">
+        <div className="flex flex-col items-center justify-start text-center md:text-left space-y-4 text-milky">
           <h1 className="text-xl font-bold tracking-tighter sm:text-2xl xl:text-3xl">
             Discover the Best Movies
           </h1>
@@ -34,7 +34,7 @@ export default async function Movies() {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-4">
         {movies.map((movie: any) => (
-          <div className="relative overflow-hidden rounded-[14px] group bg-gradient-to-t from-slate-600 to-[#292727] shadow-md p-5 cursor-pointer" key={movie.id}>
+          <div className="relative overflow-hidden rounded-[14px] group bg-gradient-to-t from-slate-600 to-[#292727] shadow-md p-5 cursor-pointer hover:-translate-y-2 hover:shadow-2xl hover:shadow-violet-500/50 transition-all" key={movie.id}>
             <Image
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
@@ -47,7 +47,7 @@ export default async function Movies() {
               <p className="text-sm text-muted-foreground line-clamp-2 text-slate-950">
                 {movie.overview}
               </p>
-              <button className="bg-marimo hover:bg-slate-900 text-white p-2 text-sm mt-3 rounded-md w-[120px]">More Info</button>
+              <button className="bg-slate-900 hover:bg-slate-800 text-milky p-2 text-sm mt-3 rounded-md w-[120px]">More Info</button>
             </div>
           </div>
         ))}
--- src/app/tvshows/page.tsx
+++ src/app/tvshows/page.tsx
@@ -33,7 +33,7 @@ export default async function TvShows() {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-4">
         {tvshows.map((tv: any) => (
-          <div className="relative overflow-hidden rounded-lg group bg-gradient-to-t from-slate-800 to-slate-400 shadow-md p-5 cursor-pointer" key={tv.id}>
+          <div className="relative overflow-hidden rounded-[14px] group bg-gradient-to-t from-slate-600 to-[#292727] shadow-md p-5 cursor-pointer hover:-translate-y-2 hover:shadow-2xl hover:shadow-violet-500/50 transition-all" key={tv.id}>
             <Image
               src={`https://image.tmdb.org/t/p/w500${tv.poster_path}`}
               alt={tv.title}
@@ -46,7 +46,7 @@ export default async function TvShows() {
               <p className="text-sm text-muted-foreground line-clamp-2">
                 {tv.overview}
               </p>
-              <button className="bg-marimo hover:bg-slate-900 text-white p-2 text-sm mt-3 rounded-md w-[120px]">More Info</button>
+              <button className="bg-slate-900 hover:bg-slate-800 text-milky p-2 text-sm mt-3 rounded-md w-[120px]">More Info</button>
             </div>
           </div>
         ))}
